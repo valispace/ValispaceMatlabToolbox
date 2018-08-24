61c5ed0fcddc0b28619e1326d5e47fa902f8c3ef
Louise Lindblad <louise@valispace.com>
2018-08-24 14:27:07 +0100

    updated matlab plugin

--- ValispacePushValue.m
+++ ValispacePushValue.m
@@ -1,6 +1,5 @@
 function [ ExecutionStatus ] = ValispacePushValue(name_or_id, value)
 % pushes a Value to Valispace
-% TBD also 
 
     global ValispaceLogin
     
@@ -14,12 +13,27 @@ function [ ExecutionStatus ] = ValispacePushValue(name_or_id, value)
     end
     
     write_options = ValispaceLogin.options;
-    write_options.RequestMethod = "patch";
+    write_options.RequestMethod = "put";
     write_options.MediaType = "application/json";
     
     url = ValispaceLogin.url + "vali/" + string(name_or_id) + "/";
        
-    % set new formula
+    read_vali = webread(url, ValispaceLogin.options);
+    
+    write_vali = read_vali;
+     
+    % remove read-only fields
+    write_vali = rmfield(write_vali,{'id','path','name','value','uses_default_formula','totalmargin_plus','totalmargin_minus','wc_plus','wc_minus','is_part_of_linking_matrix','mathjax_formula','mathjax_formula_simple'});
+     
+	% remove empty fields
+	fields = fieldnames(write_vali);
+	for i = 1:numel(fields)
+		if isempty(write_vali.(fields{i}))
+			write_vali = rmfield(write_vali,fields(i));
+		end
+	end
+	
+	% set formula
     write_vali.formula = string(value);
 
     ReturnVali = webwrite(url,write_vali,write_options);
